1be8d0b2dcf392f5e6f542bb5b2acd1416134a82
Cat Binh Le <>
2022-05-20 14:08:53 +0700

    remove port 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 // const mongoString = process.env.DATABASE_URL;
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 mongoose.connect('mongodb+srv://catbinhle:MDBLcb2022@cluster0.oc8kn.mongodb.net/test');
 const database = mongoose.connection;
